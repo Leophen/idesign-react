5f18b37989ab12c21cc2a7c937cd2c13d6f764d2
Dorkiliu <dorkiliu@tencent.com>
2022-06-10 11:23:12 +0800

    feat: Progress 进度条

--- src/Progress/index.md
+++ src/Progress/index.md
@@ -8,9 +8,108 @@ group:
 order: 15
 ---
 
-# Progress 进度条（开发中）
+# Progress 进度条
 
 ## 基本用法
+
+通过 `percentage` 属性控制进度。
+
+```tsx
+import React from 'react';
+import { Progress } from 'idesign-react';
+
+const App = () => {
+  return (
+    <div>
+      <Progress percentage={60} />
+    </div>
+  );
+};
+
+export default App;
+```
+
+## 隐藏进度提示
+
+条形进度条通过 `labelTxt` 属性控制是否显示进度提示。
+
+```tsx
+import React from 'react';
+import { Progress } from 'idesign-react';
+
+const App = () => {
+  return (
+    <div>
+      <Progress percentage={60} labelTxt={false} />
+    </div>
+  );
+};
+
+export default App;
+```
+
+## 进度提示在内
+
+条形进度条通过 `innerLabel` 属性控制是否显示进度提示，注意使用该属性时进度条最小高度会设为 `20`。
+
+```tsx
+import React from 'react';
+import { Progress } from 'idesign-react';
+
+const App = () => {
+  return (
+    <div>
+      <Progress percentage={60} innerLabel />
+    </div>
+  );
+};
+
+export default App;
+```
+
+## 自定义进度提示
+
+通过 `label` 属性设置自定义进度提示。
+
+```tsx
+import React from 'react';
+import { Progress } from 'idesign-react';
+
+const App = () => {
+  return (
+    <div className="i-design-demo-column">
+      <Progress percentage={30} label="😄" />
+      <Progress percentage={60} label={<div>🌛</div>} />
+    </div>
+  );
+};
+
+export default App;
+```
+
+## 自定义进度条颜色
+
+通过 `color` 属性设置自定义进度条颜色。
+
+```tsx
+import React from 'react';
+import { Progress } from 'idesign-react';
+
+const App = () => {
+  return (
+    <div className="i-design-demo-column">
+      <Progress percentage={30} color="#FFCF2E" />
+      <Progress percentage={60} color="linear-gradient(90deg, rgb(0, 255, 255), rgb(255, 0, 255))" />
+    </div>
+  );
+};
+
+export default App;
+```
+
+## 自定义宽度或粗细
+
+通过 `width` 来控制条形进度条长度/环形进度条直径，通过 `strokeWidth` 属性控制进度条粗细度。
 
 ```tsx
 import React from 'react';
@@ -19,12 +118,35 @@ import { Progress } from 'idesign-react';
 const App = () => {
   return (
     <div className="idesign-demo-block-column">
-      <div className="idesign-demo-block-row">
-        <Progress>填充按钮</Progress>
-      </div>
+      <Progress percentage={30} width={300} />
+      <Progress percentage={60} strokeWidth={20} />
     </div>
   );
 };
 
 export default App;
 ```
+
+## 环形进度条
+
+通过 `type` 属性设置进度条类型，为 `circle` 时显示环形进度条。
+
+```tsx
+import React from 'react';
+import { Progress } from 'idesign-react';
+
+const App = () => {
+  return (
+    <div className="idesign-demo-block-row">
+      <Progress type="circle" percentage={0} />
+      <Progress type="circle" percentage={30} width={100} />
+      <Progress type="circle" percentage={60} strokeWidth={20} />
+      <Progress type="circle" percentage={90} label={<div>🌛</div>} />
+    </div>
+  );
+};
+
+export default App;
+```
+
+<API />
--- src/Progress/index.tsx
+++ src/Progress/index.tsx
@@ -8,33 +8,158 @@ export interface ProgressProps {
    */
   className?: string;
   /**
-   * 内容
-   */
-  children?: React.ReactNode;
-  /**
    * 自定义样式
    */
   style?: React.CSSProperties;
+  /**
+   * 进度条百分比
+   * @default 0
+   */
+  percentage?: number;
+  /**
+   * 进度条类型
+   * @default bar
+   */
+  type?: 'bar' | 'circle';
+  /**
+   * 是否显示进度提示文本
+   * @default true
+   */
+  labelTxt?: boolean;
+  /**
+   * 提示是否在进度条内部
+   * @default false
+   */
+  innerLabel?: boolean;
+  /**
+   * 自定义进度提示
+   */
+  label?: React.ReactNode;
+  /**
+   * 进度条颜色
+   * @default #3a3cf5
+   */
+  color?: string;
+  /**
+   * 进度条底色
+   * @default #ebeef5
+   */
+  backColor?: string;
+  /**
+   * 进度条长度/环形直径
+   * @default 300
+   */
+  width?: number | string;
+  /**
+   * 进度条粗细度
+   * @default 6
+   */
+  strokeWidth?: number;
 }
 
 const Progress: React.FC<ProgressProps> = (props) => {
   const {
-    children = '',
     className,
     style,
+    percentage = 0,
+    type = 'bar',
+    labelTxt = true,
+    innerLabel = false,
+    label,
+    color = '#3a3cf5',
+    backColor = '#ebeef5',
+    width = 200,
+    strokeWidth = 6,
     ...restProps
   } = props;
+
+  const progressBar = (
+    <>
+      <div
+        className={classNames(
+          'i-progress-bar',
+          innerLabel && 'i-progress-bar__has-label',
+        )}
+        style={{ width, height: strokeWidth, background: backColor }}
+      >
+        <div
+          className="i-progress-bar__inner"
+          style={{ width: `${percentage}%`, background: color }}
+        />
+        {innerLabel && (labelTxt || label) && (
+          <div className="i-progress__info">
+            {!label ? `${percentage}%` : label}
+          </div>
+        )}
+      </div>
+      {!innerLabel && (labelTxt || label) && (
+        <div className="i-progress__info">
+          {!label ? `${percentage}%` : label}
+        </div>
+      )}
+    </>
+  )
+
+  const getStyleNum = (val: string | number) => {
+    return parseInt(String(width))
+  }
+  const circle = {
+    d: getStyleNum(width),
+    r: (getStyleNum(width) / 2) - strokeWidth / 2
+  }
+
+  const progressCircle = (
+    <div
+      className={classNames(
+        'i-progress-circle',
+        innerLabel && 'i-progress-circle__has-label',
+      )}
+      style={{ width, height: width }}
+    >
+      <svg
+        width={circle.d}
+        height={circle.d}
+        viewBox={`0 0 ${circle.d} ${circle.d}`}
+      >
+        <circle
+          cx={circle.d / 2}
+          cy={circle.d / 2}
+          r={circle.r}
+          strokeWidth={strokeWidth}
+          stroke={backColor}
+          fill="none"
+        />
+        <circle
+          cx={circle.d / 2}
+          cy={circle.d / 2}
+          r={circle.r}
+          strokeWidth={strokeWidth}
+          fill="none"
+          strokeDasharray={circle.d * Math.PI}
+          strokeDashoffset={circle.d * Math.PI - circle.d * Math.PI * percentage / 100}
+          className="i-progress-circle__inner"
+          stroke={color}
+          style={{ strokeLinecap: 'round' }}
+        />
+      </svg>
+      {(labelTxt || label) && (
+        <div className="i-progress__info">
+          {!label ? `${percentage}%` : label}
+        </div>
+      )}
+    </div>
+  )
 
   return (
     <div
       className={classNames(
-        'i-template',
+        'i-progress',
         className
       )}
       style={{ ...style }}
       {...restProps}
     >
-      {children}
+      {type !== 'bar' ? progressCircle : progressBar}
     </div>
   );
 };
