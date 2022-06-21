bd1302c411c7d964e89a62c4d20e00af3bbc8e89
Dorkiliu <dorkiliu@tencent.com>
2022-06-21 16:23:21 +0800

    feat: Menu temp

--- src/Menu/index.md
+++ src/Menu/index.md
@@ -12,19 +12,92 @@ order: 1
 
 ## 基本用法
 
+使用 `Menu` 包裹菜单的每一项 `Menu.Item`：
+
 ```tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'idesign-react';
 
 const App = () => {
+  const [current, setCurrent] = useState('0')
+
+  const handleChange = (val) => {
+    console.log(val)
+    setCurrent(val)
+  }
+
   return (
-    <div className="idesign-demo-block-column">
-      <div className="idesign-demo-block-row">
-        <Menu>填充按钮</Menu>
-      </div>
-    </div>
+    <Menu current={current} onChange={handleChange}>
+      <Menu.Item value='0'>菜单1</Menu.Item>
+      <Menu.Item value='1'>菜单2</Menu.Item>
+      <Menu.Item value='2'>菜单3</Menu.Item>
+      <Menu.Item value='3'>菜单4</Menu.Item>
+    </Menu>
   );
 };
 
 export default App;
 ```
+
+## 带前后缀
+
+通过 `prefixContent` 和 `suffixContent` 分别设置导航项的前后缀：
+
+```tsx
+import React from 'react';
+import { Menu } from 'idesign-react';
+
+const App = () => {
+  const prefix = <img src="/images/logo.svg" />
+  const suffix = <div>个人中心</div>
+
+  return (
+    <Menu current='0' prefixContent={prefix} suffixContent={suffix}>
+      <Menu.Item value='0'>菜单1</Menu.Item>
+      <Menu.Item value='1'>菜单2</Menu.Item>
+      <Menu.Item value='2'>菜单3</Menu.Item>
+      <Menu.Item value='3'>菜单4</Menu.Item>
+    </Menu>
+  );
+};
+
+export default App;
+```
+
+## 垂直方向
+
+通过 `direction` 属性控制导航方向：
+
+```tsx
+import React, { useState } from 'react';
+import { Menu } from 'idesign-react';
+
+const App = () => {
+  const [current, setCurrent] = useState('0')
+
+  const handleChange = (val) => {
+    console.log(val)
+    setCurrent(val)
+  }
+
+  return (
+    <Menu
+      direction="vertical"
+      width={200}
+      current={current}
+      onChange={handleChange}
+    >
+      <Menu.Item value='0'>菜单1</Menu.Item>
+      <Menu.Item value='1'>菜单2</Menu.Item>
+      <Menu.Item value='2'>菜单3</Menu.Item>
+      <Menu.Item value='3'>菜单4</Menu.Item>
+    </Menu>
+  );
+};
+
+export default App;
+```
+
+## Menu API
+
+<API hideTitle />
--- src/Menu/index.tsx
+++ src/Menu/index.tsx
@@ -15,30 +15,118 @@ export interface MenuProps {
    * 自定义样式
    */
   style?: React.CSSProperties;
+  /**
+   * 导航宽度
+   */
+  width?: number;
+  /**
+   * 当前选中值
+   */
+  current?: string | number;
+  /**
+   * 前置内容
+   */
+  prefixContent: React.ReactNode;
+  /**
+   * 前置内容
+   */
+  suffixContent: React.ReactNode;
+  /**
+   * 导航方向
+   * @default horizontal
+   */
+  direction?: 'horizontal' | 'vertical';
+  /**
+   * 点击菜单项时触发
+   */
+  onChange?: (value: string | number) => void;
 }
 
-const Menu: React.FC<MenuProps> = (props) => {
+export interface MenuItemProps extends MenuProps {
+  /**
+   * 单项值
+   */
+  value?: string | number;
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props) => {
   const {
-    children = '',
+    children,
     className,
     style,
+    value = '',
+    current,
+    onChange
+  } = props
+
+  return (
+    <li
+      className={classNames(
+        'i-menu-item',
+        value === current && 'i-menu-item__active',
+        className
+      )}
+      style={{ ...style }}
+      onClick={() => onChange?.(value)}
+    >
+      {children}
+    </li>
+  )
+}
+
+const Menu: React.FC<MenuProps> & { Item: React.ElementType } = (props) => {
+  const {
+    children,
+    className,
+    style,
+    width,
+    current,
+    prefixContent,
+    suffixContent,
+    direction = 'horizontal',
+    onChange,
     ...restProps
   } = props;
 
   return (
     <div
       className={classNames(
-        'i-template',
+        'i-menu',
+        direction === 'vertical' && 'i-menu__vertical',
         className
       )}
-      style={{ ...style }}
+      style={{ ...style, width }}
       {...restProps}
     >
-      {children}
+      {prefixContent && (
+        <header className="i-menu__logo">
+          {prefixContent}
+        </header>
+      )}
+      <ul className="i-menu-item__wrapper">
+        {React.Children.map(children, (child) => {
+          if (!React.isValidElement(child)) {
+            return null;
+          }
+          const childProps = {
+            current,
+            onChange
+          };
+          return React.cloneElement(child, childProps);
+        })}
+      </ul>
+      {suffixContent && (
+        <footer className="i-menu__operations">
+          {suffixContent}
+        </footer>
+      )}
     </div>
-  );
+  )
 };
 
+Menu.Item = MenuItem;
+
+MenuItem.displayName = 'MenuItem';
 Menu.displayName = 'Menu';
 
 export default Menu;
