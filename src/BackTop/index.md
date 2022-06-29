57d1d1eff0cabe6a4368df5a7b329068a774586e
Dorkiliu <dorkiliu@tencent.com>
2022-06-29 10:46:48 +0800

    feat: BackTop 回到顶部

--- src/BackTop/index.md
+++ src/BackTop/index.md
@@ -8,9 +8,11 @@ group:
 order: 7
 ---
 
-# BackTop 回到顶部（开发中）
+# BackTop 回到顶部
 
 ## 基本用法
+
+对具体 `window` 滚动使用：
 
 ```tsx
 import React from 'react';
@@ -18,13 +20,95 @@ import { BackTop } from 'idesign-react';
 
 const App = () => {
   return (
-    <div className="idesign-demo-block-column">
-      <div className="idesign-demo-block-row">
-        <BackTop>填充按钮</BackTop>
-      </div>
-    </div>
+    <BackTop />
   );
 };
 
 export default App;
 ```
+
+## 具体节点滚动
+
+对具体 `DOM` 节点滚动使用：
+
+```tsx
+import React from 'react';
+import { BackTop } from 'idesign-react';
+
+const App = () => {
+  const data = Array(24).fill('item')
+  return (
+    <>
+      <BackTop
+        target={
+          () => document.getElementById('idesign-demo-scroll1')
+        }
+      />
+      <div id="idesign-demo-scroll1" style={{height: 400, overflow: 'auto'}}>
+        {data.map((item, index) => (
+          <li className="idesign-demo-itemy" key={index}>
+            {item}
+          </li>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
+```
+
+## 滚动显示起点
+
+通过 `visibleHeight` 属性控制滚动到指定高度时，才显示返回顶部按钮：
+
+```tsx
+import React from 'react';
+import { BackTop } from 'idesign-react';
+
+const App = () => {
+  const data = Array(24).fill('item')
+  return (
+    <>
+      <BackTop
+        visibleHeight={100}
+        target={
+          () => document.getElementById('idesign-demo-scroll2')
+        }
+      />
+      <div id="idesign-demo-scroll2" style={{height: 400, overflow: 'auto'}}>
+        {data.map((item, index) => (
+          <li className="idesign-demo-itemy" key={index}>
+            {item}
+          </li>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
+```
+
+## 自定义返回顶部按钮
+
+直接包裹自定义内容：
+
+```tsx
+import React from 'react';
+import { BackTop } from 'idesign-react';
+
+const App = () => {
+  return (
+    <BackTop>
+      <div>⬆️</div>
+    </BackTop>
+  );
+};
+
+export default App;
+```
+
+
+
+<API />
--- src/BackTop/index.tsx
+++ src/BackTop/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
+import _ from 'lodash';
 import './index.scss';
+import Button from '../Button'
+import Icon from '../Icon'
+import Transition from '../Transition';
 
 export interface BackTopProps {
   /**
@@ -8,33 +12,99 @@ export interface BackTopProps {
    */
   className?: string;
   /**
-   * 内容
+   * 自定义内容
    */
   children?: React.ReactNode;
   /**
    * 自定义样式
    */
   style?: React.CSSProperties;
+  /**
+   * 监听滚动的元素
+   * @default () => window
+   */
+  target?: () => HTMLElement | Window;
+  /**
+   * 显示起点高度
+   * @default 0
+   */
+  visibleHeight?: number
+  /**
+   * 是否平滑滚动
+   * @default true
+   */
+  smooth?: boolean
+  /**
+   * 点击返回顶部时触发
+   */
+  onClick?: () => void;
+  /**
+   * 滚动层节点滚动时触发
+   */
+  onScroll?: () => void;
 }
 
 const BackTop: React.FC<BackTopProps> = (props) => {
   const {
-    children = '',
     className,
+    children,
     style,
+    target = () => window,
+    visibleHeight = 0,
+    smooth = true,
+    onClick = () => { },
+    onScroll = () => { },
     ...restProps
   } = props;
+
+  const [visible, setVisible] = useState(visibleHeight > 0 ? false : true);
+
+  // 获取滚动层节点 t
+  const getTarget = (target: HTMLElement | Window): HTMLElement => {
+    return target === window ? document.documentElement : (target as HTMLElement);
+  };
+
+  const handleScroll = _.throttle((e: any) => {
+    const top = (e.target as HTMLElement).scrollTop
+    top >= visibleHeight ? setVisible(true) : setVisible(false)
+    onScroll?.()
+  }, 16)
+
+  useEffect(() => {
+    const t = getTarget(target?.())
+    t?.addEventListener('scroll', handleScroll)
+  }, [])
+
+  const scrollToTop = () => {
+    const t = getTarget(target?.())
+    t?.scrollTo({
+      top: 0,
+      behavior: smooth ? 'smooth' : 'auto'
+    });
+    onClick?.();
+  };
 
   return (
     <div
       className={classNames(
-        'i-template',
+        'i-back-top',
         className
       )}
       style={{ ...style }}
+      onClick={scrollToTop}
       {...restProps}
     >
-      {children}
+      <Transition
+        timeout={200}
+        in={visible}
+        animation='fade-in'
+      >
+        {children || (
+          <Button shape="circle" className='i-back-top--button'>
+            <Icon name="ArrowUpBold" />
+          </Button>
+        )}
+      </Transition>
     </div>
   );
 };
