96e4ac3d9966d259c1f7c19c4af7cf74680ef2a2
Dorkiliu <dorkiliu@tencent.com>
2022-07-09 14:42:35 +0800

    feat: Table 表格初始化

--- src/Table/index.md
+++ src/Table/index.md
@@ -10,21 +10,80 @@ order: 11
 
 # Table 表格（开发中）
 
+用于数据收集展示、分析整理、操作处理的表格。
+
 ## 基本用法
+
+通过 `columns` 属性指定列配置，通过 `data` 属性指定数据源：
 
 ```tsx
 import React from 'react';
 import { Table } from 'idesign-react';
 
 const App = () => {
+  const columns = [
+    {
+      title: 'Name',
+      key: 'name'
+    },
+    {
+      title: 'Salary',
+      key: 'salary'
+    },
+    {
+      title: 'Address',
+      key: 'address'
+    },
+    {
+      title: 'Email',
+      key: 'email'
+    }
+  ];
+
+  const data = [
+    {
+      key: '1',
+      name: 'Jane Doe',
+      salary: 23000,
+      address: '32 Park Road, London',
+      email: 'jane.doe@example.com'
+    },
+    {
+      key: '2',
+      name: 'Alisa Ross',
+      salary: 25000,
+      address: '35 Park Road, London',
+      email: 'alisa.ross@example.com'
+    },
+    {
+      key: '3',
+      name: 'Kevin Sandra',
+      salary: 22000,
+      address: '31 Park Road, London',
+      email: 'kevin.sandra@example.com'
+    },
+    {
+      key: '4',
+      name: 'Ed Hellen',
+      salary: 17000,
+      address: '42 Park Road, London',
+      email: 'ed.hellen@example.com'
+    },
+    {
+      key: '5',
+      name: 'William Smith',
+      salary: 27000,
+      address: '62 Park Road, London',
+      email: 'william.smith@example.com'
+    }
+  ];
+
   return (
-    <div className="idesign-demo-block-column">
-      <div className="idesign-demo-block-row">
-        <Table>填充按钮</Table>
-      </div>
-    </div>
+    <Table columns={columns} data={data} />
   );
 };
 
 export default App;
 ```
+
+<API />
--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import classNames from 'classnames';
 import './index.scss';
+
+export interface ColumnType {
+  /**
+   * 唯一标识
+   */
+  key: string;
+  /**
+   * 标题
+   */
+  title: string;
+  /**
+   * 宽度
+   */
+  width?: React.CSSProperties["width"];
+  /**
+   * 对齐方式
+   */
+  align?: React.CSSProperties["textAlign"];
+}
 
 export interface TableProps {
   /**
@@ -8,35 +27,148 @@ export interface TableProps {
    */
   className?: string;
   /**
-   * 内容
-   */
-  children?: React.ReactNode;
-  /**
    * 自定义样式
    */
   style?: React.CSSProperties;
+  /**
+   * 表头配置数据
+   * @default []
+   */
+  columns: ColumnType[];
+  /**
+   * 表格数据源
+   * @default []
+   */
+  data?: { [x: string]: any }[];
+  /**
+   * 表格列表高度，超出显示滚动条
+   * @default 600
+   */
+  height?: React.CSSProperties["height"];
 }
+
+export interface ColumnGroupProps {
+  /**
+   * 列数据项
+   */
+  columns: ColumnType[];
+}
+
+const ColumnGroup: React.FC<ColumnGroupProps> = (props) => {
+  const {
+    columns
+  } = props
+
+  const columnWidths = columns.map((ele) => ele.width).join("-");
+
+  const cols = useMemo(() => {
+    const cols: React.ReactElement[] = [];
+    let mustInsert = false;
+    for (let i = columns.length; i >= 0; i--) {
+      const width = columns[i] && columns[i].width;
+      if (width || mustInsert) {
+        cols.unshift(
+          <col
+            key={i}
+            style={{ width, minWidth: width, textAlign: columns[i].align }}
+          />
+        );
+        mustInsert = true;
+      }
+    }
+    return cols;
+  }, [columnWidths]);
+
+  return <colgroup>{cols}</colgroup>;
+};
+
+export interface TableHeadProps {
+  /**
+   * 列数据
+   */
+  columns: ColumnType[];
+}
+
+const TableHead: React.FC<TableHeadProps> = (props) => {
+  const {
+    columns
+  } = props
+
+  return (
+    <thead className='i-table-head'>
+      <tr className='i-table-row'>
+        {columns.map((column) => (
+          <th className="i-table-th" key={column.key}>
+            {column.title}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
+
+export interface TableBodyProps {
+  /**
+   * 数据源
+   */
+  data?: { [x: string]: any }[];
+  /**
+   * 列数据
+   */
+  columns: ColumnType[];
+}
+
+const TableBody: React.FC<TableBodyProps> = (props) => {
+  const {
+    data,
+    columns
+  } = props
+
+  return (
+    <tbody className='i-table-body'>
+      {data?.map((item) => (
+        <tr
+          className='i-table-row'
+          key={item.key}
+        >
+          {columns.map((cell) => (
+            <td className="i-table-td" key={cell.key}>
+              {item[cell.key]}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
 
 const Table: React.FC<TableProps> = (props) => {
   const {
-    children = '',
     className,
     style,
+    columns,
+    data,
+    height = 600,
     ...restProps
   } = props;
 
   return (
-    <div
+    <table
       className={classNames(
-        'i-template',
+        'i-table',
         className
       )}
       style={{ ...style }}
       {...restProps}
     >
-      {children}
-    </div>
-  );
+      <ColumnGroup columns={columns} />
+      <TableHead columns={columns}></TableHead>
+      <TableBody
+        columns={columns}
+        data={data}
+      />
+    </table>
+  )
 };
 
 Table.displayName = 'Table';
